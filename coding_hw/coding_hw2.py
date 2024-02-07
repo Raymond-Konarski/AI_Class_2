11f0b8bd58cfc80c74fb2a3b9745e8b4bf1f703a
Raymond Konarski <konarskiraymond@gmail.com>
2024-02-07 13:45:46 -0500

    basic minimax

--- coding_hw/coding_hw2.py
+++ coding_hw/coding_hw2.py
@@ -2,6 +2,30 @@ from environments.connect_four import ConnectFourState, ConnectFour
 import numpy as np
 
 
+def min_value(state: ConnectFourState, env: ConnectFour, depth: int, max_depth: int) -> [int, int]:
+    if env.is_terminal(state): return env.utility(state), None
+    if depth >= max_depth: pass  # TODO: this
+    v = np.inf
+    m = None
+    for a in env.get_actions(state):
+        v2, a2 = max_value(env.next_state(state, a), env, depth + 1)
+        if v2 > v:
+            v, m = v2, a
+    return v, m
+
+
+def max_value(state: ConnectFourState, env: ConnectFour, depth: int, max_depth: int) -> [int, int]:
+    if env.is_terminal(state): return env.utility(state), None
+    if depth >= max_depth: pass  # TODO: this
+    v = -np.inf
+    m = None
+    for a in env.get_actions(state):
+        v2, a2 = min_value(env.next_state(state, a), env, depth + 1)
+        if v2 > v:
+            v, m = v2, a
+    return v, m
+
+
 def make_move(state: ConnectFourState, env: ConnectFour) -> int:
     """
 
@@ -9,5 +33,10 @@ def make_move(state: ConnectFourState, env: ConnectFour) -> int:
     :param env: the environment
     :return: the action to take
     """
+
+    # player = state.player_turn
+    v, m = max_value(state, env, 0, 18)
+    actions = env.get_actions(state)
+    return actions[0]
 
     pass
